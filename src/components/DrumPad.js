fbbc0ee67c317df0bd935abd5a067009e499dea7
khoaHyh <khoahuynh.toronto@gmail.com>
2020-12-19 13:38:41 -0500

    Feat: Add audio tag to test sound
    Sound successfully plays through audio tag.

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import ReactAudioPlayer from 'react-audio-player';
 
 const DrumPad = ({ id, src }) => {
     return (
         <div className="drum-pad">
             <p>{id}</p>
             <p>{src}</p>
-            
+            <ReactAudioPlayer src={src} />
+            <audio controls src={src} />
         </div>
     );
 }
