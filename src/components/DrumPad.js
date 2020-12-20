88cd10dd42ee24cbb61dd61013e521e24e1ff7d0
khoaHyh <khoahuynh.toronto@gmail.com>
2020-12-20 12:31:00 -0500

    Feat: Add keyPress and onClick functionality
    Add functionality to play sound on keyPress and onClick for DrumPad.

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ReactAudioPlayer from 'react-audio-player';
+// import ReactAudioPlayer from 'react-audio-player';
 
 const drumPadStyle = {
     border: "solid 1px black",
@@ -12,19 +12,30 @@ const drumPadStyle = {
 export default class DrumPad extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            keyPressed: ""
+        this.playSound = this.playSound.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+    }
+
+    componentDidMount() {
+        document.addEventListener("keypress", this.handleKeyPress);
+    }
+
+    handleKeyPress(event) {
+        if (event.key === this.props.keyboard.toLowerCase()) {
+            this.playSound();
         }
     }
 
-    
+    playSound() {
+        this.sound.play();
+    }
 
     render() {
         const { id, keyboard, src } = this.props;
         return (
-            <button id={id} className="drum-pad" style={drumPadStyle}>
+            <button id={id} onClick={this.playSound} className="drum-pad" style={drumPadStyle}>
                 {keyboard}
-                <ReactAudioPlayer src={src} />
+                <audio id={keyboard} className="clip" ref={(element) => {this.sound = element}}  src={src} />
             </button>
         );
     }
